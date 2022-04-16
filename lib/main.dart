1cbec4b5e2f096408d4ecac28e5ded654d10b506
madhu6604 <89292188+madhu6604@users.noreply.github.com>
2022-04-16 12:40:43 +0530

    Initial commit of i am rich basic flutter app

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+//The main function is the starting point for our Flutter apps.
+void main(){
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
